175e05aee670af7e97651f049f08bc21a042c568
Jonas Harmeling <Jonas Harmeling>
2016-10-24 17:09:58 +0200

    new user input method

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -12,7 +12,12 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		HelloUser user1 = new HelloUser();
 		System.out.println("Hello ASE2016 - how are you? :)");
+		System.out.print("Please enter your name: ");
+		user1.userName = user1.askName();
+		System.out.print("Hello ");
+		System.out.println(user1.userName);
 	}
 
 }
